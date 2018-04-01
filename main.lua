2246554d7b5c36d265d60c2e92b1775c63f12b1f
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-04-01 14:09:15 -0400

    done

--- main.lua
+++ main.lua
@@ -17,29 +17,30 @@ display.setDefault("background",124/255, 15/255,110/255)
 local questionObject
 local correctObject
 local numericField
-local randomNumber1
-local randomNumber2
-local userAnswer
-local correctAnswer
+local randomNumber1 = 0
+local randomNumber2 = 0
+local userAnswer 
+local correctAnswer = 0
 local incorrectObject
-local randomOperation
-local numericField
-local score = 0
+local randomOperation = 0
 local scoreText
+local score = 0
+local scoreTimer
+
 ----------------------------------------------------------------------------
 --LOCAL FUNCTIONS
 ----------------------------------------------------------------------------
 
 local function AskQuestion()
 
-	-- program chooses a random number betweer 1-4
+	-- program chooses a random number betweer 1-3
       randomOperation = math.random(1,3)
 	-- generate 2 random numbers between a max. and a min. number
 	randomNumber1 = math.random(0,25)
 	randomNumber2 = math.random(0,21)
 	randomNumber3 = math.random(0,10)
 	randomNumber4 = math.random(0,10)
-	if ( randomOperation == 1)then correctAnswer = randomNumber1 + randomNumber2
+	if ( randomOperation == 1) then correctAnswer = randomNumber1 + randomNumber2
 
 	--create question in text object
 	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
@@ -47,9 +48,9 @@ local function AskQuestion()
 
     if (randomOperation == 2) then correctAnswer = randomNumber1 - randomNumber2
     --create question in text object
-    questionObject.text = randomNumber1 .. " - " .. randomNumber2.. " = "
-    
-    elseif (correctAnswer < 0) then
+    questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+    end
+    if ((correctAnswer < 0) and (randomOperation == 2)) then
     	correctAnswer = randomNumber2 - randomNumber1
     	questionObject.text = randomNumber2 .. " - " .. randomNumber1 .. " = "
 end
@@ -90,31 +91,25 @@ local function NumericFieldListener( event )
 			if (userAnswer == correctAnswer) then
 				correctObject.isVisible = true
 				timer.performWithDelay(1000,HideCorrect)
+				score = score + 1
+		        scoreText.text = ("score:" .. score)
+
+								
 				else 
 				incorrectObject.isVisible = true
                 timer.performWithDelay(1000,HideIncorrect)
 			end
+			
 			--clear text field
 		event.target.text = ""
 		end
 	end
 
-	
-
-local function ScoreTracker( event )
-
-local scoreTimer
+	     
 
 
-if (userAnswer == correctAnswer) then
 
-score = score + 1
 
-end
-
-scoreText.text = score .. ""
-
-end
 
 	 -----------------------------------------------------------------
 	 -- OBJECT CREATION
@@ -125,7 +120,7 @@ end
 	 questionObject:setTextColor(155/255, 150/255, 100/255)
 
 	 --displays the score
-	 scoreText = display.newText("", 500,500, nil, 40)
+	 scoreText = display.newText("score:" .. score, 500,500, nil, 40)
 
 	 --create the correct text object and make it invisible
 	 correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3,nil,50)
@@ -140,19 +135,9 @@ end
 	 numericField = native.newTextField( 500, 350, 150, 30)
 	 numericField.inputType = "number"
 
-	 
-	 -- create the score text object 
-	 score = display.newText( score, display.contentWidth/2, display.contentHeight*2/3,nil,50)
-	 score:setTextColor(100/255, 180/255, 19/255)
-	
-
-
-
-	 -- add the event listener for the numeric field
+     -- add the event listener for the numeric field
 	 numericField:addEventListener( "userInput", NumericFieldListener )
-	 -- add the event listener for the score tracker
-	 numericField:addEventListener( "userInput", ScoreTracker )
-
+	
 
 	 ---------------------------------------------------------------------
 	 --FUNCTION CALLS
